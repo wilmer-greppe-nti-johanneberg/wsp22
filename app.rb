8b172f371863bbf39563a6ba9712fab664bb2824
wilmer-greppe-nti-johanneberg <74712564+wilmer-greppe-nti-johanneberg@users.noreply.github.com>
2022-02-08 15:29:06 +0100

    Vecka 6

--- app.rb
+++ app.rb
@@ -3,6 +3,19 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+def get_database(path)
+    db = SQLite3::Database.new(path)
+    db.results_as_hash = true
+    return db
+end
+
 get('/') do
-    slim(:register)
+    slim(:start)
+end
+
+get('/games') do
+    db = get_database('db/data.db')
+    test = db.execute("SELECT * FROM games")
+    p test
+    slim(:"games/index")
 end
