5a3d66053e1b90c39ec11d936de825f8513d3bbb
wilmer-greppe-nti-johanneberg <74712564+wilmer-greppe-nti-johanneberg@users.noreply.github.com>
2022-03-01 15:35:08 +0100

    Vecka 9

--- app.rb
+++ app.rb
@@ -3,6 +3,8 @@ require 'slim'
 require 'sqlite3'
 require 'bcrypt'
 
+enable :sessions
+
 def get_database(path)
     db = SQLite3::Database.new(path)
     db.results_as_hash = true
@@ -18,4 +20,26 @@ get('/games') do
     test = db.execute("SELECT * FROM games")
     p test
     slim(:"games/index")
+end
+
+post('/users/new') do
+    username = params[:username]
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    usertype = params[:usertype]
+    db = get_database('db/data.db')
+
+    result = db.execute("SELECT id FROM users WHERE username=?", username)
+
+    if result.empty?
+        if password == password_confirm
+            password_digest = BCrypt::Password.create(password)
+            db.execute("INSERT INTO users (username,password,usertype) VALUES(?,?,?)", username,password_digest,usertype)
+            redirect('/')
+        else
+            "Ett fel uppstod: Lösenorden matchade inte!"
+        end
+    else
+        "Ett fel uppstod: Användaren finns redan!"
+    end
 end
